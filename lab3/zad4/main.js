f4d9deb245b7aea48637c0d9a573dccd4deda6ff
Pirgo <pierogmateusz@wp.pl>
2020-11-10 19:21:18 +0100

    fixed

--- lab3/zad4/main.js
+++ lab3/zad4/main.js
@@ -1,11 +1,11 @@
 var list = [];
-const userList = document.getElementById('arr');
+// const userList = document.getElementById('arr');
 
 function addFunction(){
     const value = document.getElementById('whattoadd').value
     list.push(value)
     const li = document.createElement('li')
-    li.appendChild(document.createTextNode(list));
+    li.appendChild(document.createTextNode(list))
     arr.appendChild(li)
     console.log(list)
     document.getElementById('whattoadd').value = ''
@@ -15,6 +15,6 @@ function removeFunction(){
     list.shift()
     console.log(list)
     const li = document.createElement('li')
-    li.appendChild(document.createTextNode(list));
+    li.appendChild(document.createTextNode(list))
     arr.appendChild(li)
 }
