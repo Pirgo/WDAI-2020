8441f0a33d8f44b91eef0a78df51d8c183c5c494
Pirgo <pierogmateusz@wp.pl>
2020-11-10 17:40:36 +0100

    added zad3

---
+++ lab3/zad3/main.js
@@ -0,0 +1,21 @@
+function incrementValue(){
+    let value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+}
+
+
+function toggler(){
+    const currentvalue = document.getElementById('onoff').value
+    if(currentvalue === 'ON'){
+        document.getElementById('number').value = 0
+        document.getElementById('onoff').value = 'OFF'
+        document.getElementById('cnt').disabled = false
+    }
+    else{
+        document.getElementById('number').value = 0
+        document.getElementById('onoff').value = 'ON'
+        document.getElementById('cnt').disabled = true
+    }
+}
