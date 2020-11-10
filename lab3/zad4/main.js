d9a71f9433ada767b7fd03e7dbdad4bac5863862
Pirgo <pierogmateusz@wp.pl>
2020-11-10 19:15:54 +0100

    added zad4

---
+++ lab3/zad4/main.js
@@ -0,0 +1,20 @@
+var list = [];
+const userList = document.getElementById('arr');
+
+function addFunction(){
+    const value = document.getElementById('whattoadd').value
+    list.push(value)
+    const li = document.createElement('li')
+    li.appendChild(document.createTextNode(list));
+    arr.appendChild(li)
+    console.log(list)
+    document.getElementById('whattoadd').value = ''
+}
+
+function removeFunction(){
+    list.shift()
+    console.log(list)
+    const li = document.createElement('li')
+    li.appendChild(document.createTextNode(list));
+    arr.appendChild(li)
+}
